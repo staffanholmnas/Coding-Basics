41f09376ab4b6fbdc4eb00c8d1908b7dd747eb27
staffanholmnas <staffan.holmnas@centria.fi>
2020-01-26 23:09:11 +0200

    Exercise 47 - 48 Done

--- part2/moreLoops/exercise_47/Program.cs
+++ part2/moreLoops/exercise_47/Program.cs
@@ -8,7 +8,16 @@ namespace exercise_47
     {
 
       // Write your code here:
+      Console.WriteLine("Where to?"); // Asks for an input.
+      int lastnumber = Convert.ToInt32(Console.ReadLine()); // This is the last number.
       
+      Console.WriteLine("Where from?"); // Asks for another input.
+      int firstnumber = Convert.ToInt32(Console.ReadLine()); // This is the first number.
+
+      for (int i = firstnumber; i <= lastnumber; i++) // Loop prints every number between first and last.
+      {
+          Console.WriteLine(i);
+      }
     }
   }
 }
--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -8,7 +8,47 @@ namespace exercise_48
     {
 
       // Write your code here:
- 
+      Console.WriteLine("Give numbers:"); // Prints a string asking for numbers.
+      
+      // Declares variables and sets values to zero.
+      double sum = 0; 
+      int numberOfInputs = 0;
+      int even = 0;
+      int odd = 0;
+
+      // A while loop is started, ends when user input is -1.
+      while (true)
+      {
+        int input = Convert.ToInt32(Console.ReadLine()); // Input variable is converted to integer.
+
+        if (input == -1) // Breaks the loop if input is -1.
+        {
+          break;
+        }
+
+        sum += input; // Adds up the inputs and names the variable sum.
+        numberOfInputs++; // Adds 1 everytime the loop runs and stores the value in a variable.
+
+        if (input%2 == 0) // Divides input by 2 and if the remainder is equal to zero, then adds 1 to variable even.
+        {
+          even++;
+        }
+        
+        else // Otherwise the input must be odd and adds 1 to the variable odd.
+        {
+          odd++;
+        }
+      }
+
+      double average = sum/numberOfInputs; // The average is calculated by dividing sum by the number of inputs.
+
+      // Somve messages are printed along with all of the variables.
+      Console.WriteLine("Thx! Bye!");
+      Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Numbers: " + numberOfInputs);
+      Console.WriteLine("Average: " + average);
+      Console.WriteLine("Even: " + even);
+      Console.WriteLine("Odd: " + odd);
     }
   }
 }
