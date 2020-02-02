874facaca0ddc68a2282c5a3b17d00cacfe00c9b
staffanholmnas <staffan.holmnas@centria.fi>
2020-02-02 15:16:55 +0200

    Exercise 53 hacked

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -14,7 +14,10 @@ namespace exercise_53
     // Write your method here:
     public static void Division(int numerator, int denominator)
     {
-      Console.WriteLine((double)numerator/denominator); // The numbers are divided and the rusult is printed.
+      double answer = (double)numerator/denominator; // The numbers are divided and the rusult is printed.
+      string stringAnswer = Convert.ToString(answer); // In order to pass the test, the answer is converted to string and
+      string dotReplaced = stringAnswer.Replace(",", "."); // the comma is replaced with a dot.
+      Console.WriteLine(dotReplaced);
     }
   }
 }
