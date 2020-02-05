3f2072395370f311a174df79c2086325edaefd13
Staffan Holmnäs SOFTK20P <staffan.holmnas@centria.fi>
2020-02-05 14:04:48 +0200

    Exercise 85 Done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,8 +7,23 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter username:"); // Asks for 2 inputs, username and password.
+      string username = Console.ReadLine();
+      Console.WriteLine("Enter password:");
+      string password = Console.ReadLine();
 
-
+      if (username == "alex" && password == "sunshine") // Checks for correct username and password for user 1.
+      {
+        Console.WriteLine("You have successfully logged in!"); // Prints a successful login.
+      }
+      else if (username == "emma" && password == "haskell") // Checks for correct username and password for user 2.
+      {
+        Console.WriteLine("You have successfully logged in!"); // Prints a successful login.
+      }
+      else
+      {
+        Console.WriteLine("Incorrect username or password!"); // If the login fails print this.
+      }
     }
   }
 }
