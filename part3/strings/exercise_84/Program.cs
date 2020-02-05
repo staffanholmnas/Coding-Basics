9ae5e11ca151e01dcec0af7cc971aa2382f7c75f
Staffan Holmnäs SOFTK20P <staffan.holmnas@centria.fi>
2020-02-05 11:54:10 +0200

    Exercise 84 Done

--- part3/strings/exercise_84/Program.cs
+++ part3/strings/exercise_84/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_84
   {
     public static void Main(string[] args)
     {
+      Console.Write("Give a string: "); // Asks for a string.
 
+      string message = Console.ReadLine(); // Input a string.
+
+      if (message == "true") // If your string says "true", then you got it right.
+      {
+        Console.WriteLine("You got it right!");
+      }
+      else
+      {
+        Console.WriteLine("Try again!");
+      }
 
     }
   }
