6a4970b6a150415803dae97e9b754d37a18627a6
Staffan Holmnäs SOFTK20P <staffan.holmnas@centria.fi>
2020-02-06 13:54:37 +0200

    Exercise 92 started

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 
 namespace exercise_92
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
+            int oldest = 0;
+            int length = 0;
+            string longestName = "";
+            int age = 0;
+            int ageCompare = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                string[] parts = input.Split(",");
+                if (Convert.ToInt32(parts[1]) > oldest)
+                {
+                    oldest = Convert.ToInt32(parts[1]);
+                    
+                }
+                string word = parts[0];
+                length = word.Length;
+
+                if (length > ageCompare)
+                {
+                
+                longestName = parts[0];
+                length = ageCompare;
+                }
+               
+            }
+            age = 2020 - oldest;
+            Console.WriteLine("Longest name: " + longestName);
+            Console.WriteLine("Highest age: " + age);
+        }
     }
-  }
 }
 
 
