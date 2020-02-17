7c15b87607b730519c6723059469d60ad84c0937
staffanholmnas <staffan.holmnas@centria.fi>
2020-02-17 23:48:09 +0200

    Exercise 124 Done

--- part5/references/exercise_124/Program.cs
+++ part5/references/exercise_124/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace exercise_124
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Implement your program here!
+        public static void Main(string[] args)
+        {
+            // Implement your program here!
 
-      
+          List<int> list = new List<int>(); // Creates a new list object.
+
+         list = null; // The list is nulled.
+         list.ForEach(Console.WriteLine); // Tries to do something with the list, but we get a NullReferenceException error instead.
+
+        }
     }
-  }
 }
