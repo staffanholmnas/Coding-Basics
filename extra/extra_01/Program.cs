6ed152ca75080f951088239228fe4279dc5d4128
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-10 22:21:16 +0200

    Extra_01 Done

--- extra/extra_01/Program.cs
+++ extra/extra_01/Program.cs
@@ -2,12 +2,24 @@
 
 namespace extra_01
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            Console.WriteLine("What do you want to print?");
+            string printThis = Console.ReadLine();
+            Console.WriteLine("How many times do you want to print?");
+            int numberOfPrints = Convert.ToInt32(Console.ReadLine());
 
+            Printer(numberOfPrints, printThis);
+        }
+        static void Printer(int printTimes, string whatToPrint)
+        {
+            for (int i = 0; i < printTimes; i++)
+            {
+                Console.WriteLine(whatToPrint);
+            }
+        }
     }
-  }
 }
