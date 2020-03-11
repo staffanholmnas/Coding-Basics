978751864eef4ea030c5bc52473f7439adc50d1b
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-11 22:08:56 +0200

    Extra_04 Done

--- extra/extra_04/Program.cs
+++ extra/extra_04/Program.cs
@@ -7,7 +7,40 @@ namespace extra_04
     public static void Main(string[] args)
     {
       // Add your code here:
-
+        Console.WriteLine("Give your percentage [0-100]:");
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input <= 0)
+        {
+            Console.WriteLine("Impossible");
+        }
+        else if (input <= 49)
+        {
+            Console.WriteLine("Fail");
+        }
+        else if (input <= 59)
+        {
+            Console.WriteLine("Grade: 1");
+        }
+        else if (input <= 69)
+        {
+            Console.WriteLine("Grade: 2");
+        }
+        else if (input <= 79)
+        {
+            Console.WriteLine("Grade: 3");
+        }
+        else if (input <= 89)
+        {
+            Console.WriteLine("Grade: 4");
+        }
+        else if (input <= 100)
+        {
+            Console.WriteLine("Grade: 5");
+        }
+        else
+        {
+            Console.WriteLine("Outstanding!");
+        }
     }
   }
 }
