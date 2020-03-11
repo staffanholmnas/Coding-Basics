da878d37f1be53c6d9b4a3b51948f0c1efe6116b
Staffan Holmnäs SOFTK20P <staffan.holmnas@centria.fi>
2020-03-11 13:52:07 +0200

    Extra_03 Done

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_03
 {
@@ -7,7 +8,28 @@ namespace extra_03
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      List<int> list = new List<int>();
+      Console.WriteLine("How many numbers?");
+      int numberOfInputs = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give " + numberOfInputs + " numbers:");
+      int number = 0;
+      double sum = 0;
+      int total = 1;
+      double average = 0;
+      for (int i = 0; i < numberOfInputs; i++)
+      {
+          number = Convert.ToInt32(Console.ReadLine());
+          list.Add(number);
+      }
+      foreach (int item in list)
+      {
+          sum += item;
+          total = total * item;
+      }
+      average = sum / numberOfInputs;
+      Console.WriteLine("Their sum: " + sum);
+      Console.WriteLine("Their total: " + total);
+      Console.WriteLine("Their average: " + average);
     }
   }
 }
