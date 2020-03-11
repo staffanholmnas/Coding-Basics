91c0a13b12c149fb82dd203856498debd042e157
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-11 22:17:53 +0200

    Extra_05 Done

--- extra/extra_05/Program.cs
+++ extra/extra_05/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_05
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
-
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            List<string> list = new List<string>();
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Give words, 'end' quits:");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
-  }
 }
