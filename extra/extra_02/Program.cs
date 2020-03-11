e54cf07bde4e654090029636dcbef2ed331ec239
Staffan Holmnäs SOFTK20P <staffan.holmnas@centria.fi>
2020-03-11 13:24:39 +0200

    Extra_02 Done

--- extra/extra_02/Program.cs
+++ extra/extra_02/Program.cs
@@ -7,7 +7,24 @@ namespace extra_02
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      Console.WriteLine("Give three numbers:");
+      int number1 = Convert.ToInt32(Console.ReadLine());
+      int number2 = Convert.ToInt32(Console.ReadLine());
+      int number3 = Convert.ToInt32(Console.ReadLine());
+      int largest = 0;
+      if (number1 > number2 && number1 > number3)
+      {
+          largest = number1;
+      }
+      else if (number2 > number1 && number2 > number3)
+      {
+          largest = number2;
+      }
+      else
+      {
+          largest = number3;
+      }
+      Console.WriteLine("Largest: " + largest);
     }
   }
 }
