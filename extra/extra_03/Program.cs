4666a9d6f77e43c33cd62a3a28a600903eff5a4a
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-11 21:59:03 +0200

    Extra_03 Done

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace extra_03
 {
@@ -8,25 +7,26 @@ namespace extra_03
     public static void Main(string[] args)
     {
       // Add your code here:
-      List<int> list = new List<int>();
+      
       Console.WriteLine("How many numbers?");
       int numberOfInputs = Convert.ToInt32(Console.ReadLine());
       Console.WriteLine("Give " + numberOfInputs + " numbers:");
+      
       int number = 0;
       double sum = 0;
       int total = 1;
       double average = 0;
-      for (int i = 0; i < numberOfInputs; i++)
+
+      while (number < numberOfInputs)
       {
-          number = Convert.ToInt32(Console.ReadLine());
-          list.Add(number);
+        int input = Convert.ToInt32(Console.ReadLine());
+        sum += input;
+        total = total * input;
+        number++;
       }
-      foreach (int item in list)
-      {
-          sum += item;
-          total = total * item;
-      }
+
       average = sum / numberOfInputs;
+      
       Console.WriteLine("Their sum: " + sum);
       Console.WriteLine("Their total: " + total);
       Console.WriteLine("Their average: " + average);
