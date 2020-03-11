97fbb136328972910a2fad29c07825612814ea18
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-11 22:28:35 +0200

    Extra_06 Done

--- extra/extra_06/Program.cs
+++ extra/extra_06/Program.cs
@@ -2,12 +2,21 @@
 
 namespace extra_06
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
-
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            if (number1 + number2 < 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(Math.Sqrt(number1 + number2));
+            }
+        }
     }
-  }
 }
