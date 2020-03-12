641c59867158e21390f94142a99e400a086949bd
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-12 20:05:08 +0200

    Extra_09 Done

--- extra/extra_09/Program.cs
+++ extra/extra_09/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_09
 {
@@ -7,7 +8,21 @@ namespace extra_09
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      List<string> list = new List<string>() { "This is a string", "This is magic", "THIS IS PATRICK!" };
+      for (int i = 0; i < list.Count; i++)
+      {
+          Console.WriteLine(list[i]);
+      }
+      int index = 0;
+      while (index < list.Count)
+      {
+          Console.WriteLine(list[index]);
+          index++;
+      }
+      foreach (string item in list)
+      {
+          Console.WriteLine(item);
+      }
     }
   }
 }
