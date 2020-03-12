090f25eaf6c3dcef08ee25965e04be3ceaef041b
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-12 11:10:27 +0200

    Extra_07 Done

--- extra/extra_07/Program.cs
+++ extra/extra_07/Program.cs
@@ -2,12 +2,24 @@
 
 namespace extra_07
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
-
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            int sum = 0;
+            Console.WriteLine("Give intgers, 'end' quits:");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                int numbers = Convert.ToInt32(input);
+                sum += numbers;
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
     }
-  }
 }
