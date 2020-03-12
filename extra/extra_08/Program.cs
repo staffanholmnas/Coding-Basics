c8baf8bbaf03fb4a918371c5e30eaaf08effaf61
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-12 19:31:09 +0200

    Extra_08 Done

--- extra/extra_08/Program.cs
+++ extra/extra_08/Program.cs
@@ -2,12 +2,40 @@
 
 namespace extra_08
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
-
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            ChristmasTree(10);
+        }
+        public static void ChristmasTree(int height)
+        {
+          for (int i = 0; i <= height; i++)
+          {
+              printSpaces(height - i);
+              printStars((i * 2) - 1);
+              Console.WriteLine();
+          }
+          printSpaces(height-2);
+          printStars(3);
+          Console.WriteLine();
+          printSpaces(height-2);
+          printStars(3);  
+        }
+        public static void printStars(int numberOfStars)
+        {
+            for (int i = 1; i <= numberOfStars; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        public static void printSpaces(int numberOfSpaces)
+        {
+            for (int i = 0; i <= numberOfSpaces; i++)
+            {
+                Console.Write(" ");
+            }
+        }
     }
-  }
 }
