b8be3b3c290fb34923d1ffbe41b4b09dfae1cbeb
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-15 20:02:17 +0200

    Extra_10 Done

--- extra/extra_10/Program.cs
+++ extra/extra_10/Program.cs
@@ -2,12 +2,29 @@
 
 namespace extra_10
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            int[] array = { 5, 1, 3, 4, 2 };
+            PrintArrayInStars(array);
+        }
+        public static void PrintArrayInStars(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                PrintStars(array[i]);
+            }
+        }
 
+        public static void PrintStars(int numberOfStars)
+        {
+            for (int i = 0; i < numberOfStars; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
     }
-  }
 }
