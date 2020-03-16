61599e2e7ff7670341a4978fa0dfd66cdd31e8cb
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-16 20:00:16 +0200

    Extra_13 Done

--- extra/extra_13/Program.cs
+++ extra/extra_13/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_13
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            List<Person> kindergarten = new List<Person>();
 
+            Person mike = new Person("Mike");
+            Person lily = new Person("Lily");
+
+            kindergarten.Add(mike);
+            kindergarten.Add(lily);
+
+            lily.GrowOlder(1);
+
+            foreach (Person child in kindergarten)
+            {
+                Console.WriteLine(child);
+            }
+        }
     }
-  }
 }
