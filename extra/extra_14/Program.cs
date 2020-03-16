c7a45c20b3b4c751d86aa9a7ab491e46b43c7b0c
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-16 20:11:39 +0200

    Extra_14 Done

--- extra/extra_14/Program.cs
+++ extra/extra_14/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.IO;
 
 namespace extra_14
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Add your code here:
-
+        public static void Main(string[] args)
+        {
+            // Add your code here:
+            string text = File.ReadAllText("text.txt");
+            Console.WriteLine(text);
+        }
     }
-  }
 }
