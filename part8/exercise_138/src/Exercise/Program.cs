b2d5eba9881c627009b5aaf1013a6c1113a8159b
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-30 13:39:19 +0300

    Exercise 138 Done

--- part8/exercise_138/src/Exercise/Program.cs
+++ part8/exercise_138/src/Exercise/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      // Do something here
+        public static void Main(string[] args)
+        {
+            // Do something here
+            Dictionary<string, string> dict = new Dictionary<string, string>();
 
-      }
+            dict.Add("matthew", "matt");
+            dict.Add("michael", "mix");
+            dict.Add("arthur", "artie");
+
+            foreach (KeyValuePair<string, string> item in dict)
+            {
+                Console.WriteLine("{0}'s nickname is {1}", item.Key, item.Value);
+            }
+        }
     }
-  }
 }
