622530897e31e17f6dc3a699a03eec1efb9a753c
staffanholmnas <staffan.holmnas@centria.fi>
2020-03-31 19:10:53 +0300

    Exercise 142 Done

--- part8/exercise_142/src/Exercise/IOU.cs
+++ part8/exercise_142/src/Exercise/IOU.cs
@@ -1,17 +1,51 @@
-
+using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class IOU
-  {
-    public void ChangeDebt(string toWhom, int amount)
+    public class IOU
     {
-
+        private Dictionary<string, int> dictionary;
+        public IOU()
+        {
+            this.dictionary = new Dictionary<string, int>();
+        }
+        public void ChangeDebt(string toWhom, int amount)
+        {
+            if (this.dictionary.ContainsKey(toWhom))
+            {
+                // If the person is already on the debt list, add the old debt to the new debt. 
+                // Then remove the key/value element from the dictionary and add a new key/value pair. 
+                // The debt can't be negative in any case.
+                int newAmount = this.dictionary[toWhom] + amount;
+                this.dictionary.Remove(toWhom);
+                if (newAmount >= 0)
+                {
+                    this.dictionary.Add(toWhom, newAmount);
+                }
+                else
+                {
+                    this.dictionary.Add(toWhom, 0);
+                }
+            }
+            else if (amount >= 0)
+            {
+                this.dictionary.Add(toWhom, amount);
+            }
+            else
+            {
+                this.dictionary.Add(toWhom, 0);
+            }
+        }
+        public int HowMuchDoIOweTo(string toWhom)
+        {
+            if (this.dictionary.ContainsKey(toWhom))
+            {
+                return this.dictionary[toWhom];
+            }
+            else
+            {
+                return 0; // If the person doesn't exist, return zero instead.
+            }
+        }
     }
-
-    public int HowMuchDoIOweTo(string toWhom)
-    {
-      return 0;
-    }
-  }
 }
--- part8/exercise_142/src/Exercise/Program.cs
+++ part8/exercise_142/src/Exercise/Program.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      IOU mattsIOU = new IOU();
-      mattsIOU.ChangeDebt("Arthur", 51);
-      mattsIOU.ChangeDebt("Arthur", 30);
+        public static void Main(string[] args)
+        {
+            IOU mattsIOU = new IOU();
+            mattsIOU.ChangeDebt("Arthur", -10);
 
-      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+            Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
 
-      mattsIOU.ChangeDebt("Arthur", -30);
+            mattsIOU.ChangeDebt("Arthur", 51);
+            mattsIOU.ChangeDebt("Arthur", 30);
 
-      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+            Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
 
-      mattsIOU.ChangeDebt("Arthur", -80);
+            mattsIOU.ChangeDebt("Arthur", -30);
 
-      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+            Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+
+            mattsIOU.ChangeDebt("Arthur", -80);
+
+            Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+        }
     }
-  }
 }
