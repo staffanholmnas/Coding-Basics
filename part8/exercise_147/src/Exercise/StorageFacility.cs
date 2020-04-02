577399d42282513c48a35178e341b03d06073cdf
staffanholmnas <staffan.holmnas@centria.fi>
2020-04-02 19:34:50 +0300

    Part 8 Done

--- part8/exercise_147/src/Exercise/StorageFacility.cs
+++ part8/exercise_147/src/Exercise/StorageFacility.cs
@@ -3,26 +3,50 @@ using System;
 
 namespace Exercise
 {
-  public class StorageFacility
-  {
-
-    public void Add(string unit, string item)
+    public class StorageFacility
     {
+        private Dictionary<string, List<string>> storage;
+        public StorageFacility()
+        {
+            this.storage = new Dictionary<string, List<string>>();
+        }
 
+        public void Add(string unit, string item)
+        {
+            if (!this.storage.ContainsKey(unit))
+            {
+                this.storage.Add(unit, new List<string>());
+            }
+            this.storage[unit].Add(item);
+        }
+
+        public List<string> Contents(string storageUnit)
+        {
+            if (!this.storage.ContainsKey(storageUnit))
+            {
+                this.storage.Add(storageUnit, new List<string>());
+            }
+            return this.storage[storageUnit];
+        }
+
+        public void Remove(string storageUnit, string item)
+        {
+            this.storage[storageUnit].Remove(item);
+            if (this.storage[storageUnit].Count == 0)
+            {
+                this.storage.Remove(storageUnit);
+            }
+        }
+
+        public List<string> StorageUnits()
+        {
+            List<string> listOfStorageUnits = new List<string>();
+            Dictionary<string, List<string>>.KeyCollection keys = this.storage.Keys;
+            foreach (string storages in keys)
+            {
+              listOfStorageUnits.Add(storages);
+            }
+            return listOfStorageUnits;
+        }
     }
-
-    public List<string> Contents(string storageUnit)
-    {
-      return new List<string>();
-    }
-
-    public void Remove(string storageUnit, string item)
-    {
-
-    }
-
-    public List<string> StorageUnits()
-    {
-      return new List<string>();
-    }
-  }
+}
