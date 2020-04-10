c4b2f91045ef8af7188f8bb33140fddb39e8081b
staffanholmnas <staffan.holmnas@centria.fi>
2020-04-10 15:48:40 +0300

    Exercise 150 Done

--- part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
@@ -1,39 +1,56 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
-  public class ChangeHistory
-  {
-    private List<int> history;
-
-    public ChangeHistory()
+    using System.Linq;
+    using System.Collections.Generic;
+    public class ChangeHistory
     {
+        private List<int> history;
 
+        public ChangeHistory()
+        {
+            this.history = new List<int>();
+        }
+
+        public void Add(int status)
+        {
+            this.history.Add(status);
+        }
+
+        public void Clear()
+        {
+            this.history.Clear();
+        }
+
+        public int MaxValue()
+        {
+            int maxValue = 0;
+            if (this.history.Count > 0)
+            {
+                maxValue = this.history.Max();
+            }
+            return maxValue;
+        }
+
+        public int MinValue()
+        {
+            int minValue = 0;
+            if (this.history.Count > 0)
+            {
+                minValue = this.history.Min();
+            }
+            return minValue;
+        }
+
+        public override string ToString()
+        {
+            //  This would also get the last value:
+            //  int current = 0;
+            //  foreach (var item in this.history)
+            //    {
+            //      current = item;
+            //    } 
+            
+            return "Current: " + this.history.LastOrDefault() + " Min: " + MinValue() + " Max: " + MaxValue();
+        }
     }
-
-    public void Add(int status)
-    {
-
-    }
-
-    public void Clear()
-    {
-
-    }
-
-    public int MaxValue()
-    {
-      return 0;
-    }
-
-    public int MinValue()
-    {
-      return 0;
-    }
-
-    public override string ToString()
-    {
-      return "";
-    }
-  }
 }
--- part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
@@ -1,25 +1,32 @@
 namespace Exercise
 {
-  public class ProductWarehouseWithHistory : ProductWarehouse
-  {
-    public ProductWarehouseWithHistory(string productName, int capacity, int initialBalance) : base(productName, capacity)
+    public class ProductWarehouseWithHistory : ProductWarehouse
     {
+        public ChangeHistory listOfChanges;
+        public int initialBalance;
+        public ProductWarehouseWithHistory(string productName, int capacity, int initialBalance) : base(productName, capacity)
+        {
+            this.initialBalance = initialBalance;
+            this.listOfChanges = new ChangeHistory();
+            AddToWarehouse(initialBalance);
+        }
 
+        public string History()
+        {
+            return this.listOfChanges.ToString();
+        }
+
+        new public void AddToWarehouse(int amount)
+        {
+          base.AddToWarehouse(amount);
+          this.listOfChanges.Add(this.balance);
+        }
+        
+        new public int TakeFromWarehouse(int amount)
+        {
+          base.TakeFromWarehouse(amount);
+          this.listOfChanges.Add(this.balance);
+          return amount;
+        }
     }
-
-    public string History()
-    {
-      return "";
-    }
-
-    new public void AddToWarehouse(int amount)
-    {
-
-    }
-
-    new public int TakeFromWarehouse(int amount)
-    {
-      return 0;
-    }
-  }
 }
