cfd82e5f7bec1629e75ccc214a5c1f7ae2c1f6b0
staffanholmnas <staffan.holmnas@centria.fi>
2020-04-16 16:26:47 +0300

    Exercise 159 Done

--- part10/exercise_159/src/Exercise/Program.cs
+++ part10/exercise_159/src/Exercise/Program.cs
@@ -2,23 +2,23 @@
 
 namespace Exercise
 {
-  using System;
-  using System.IO;
-  using System.Collections.Generic;
-  public class Program
-  {
-    public static void Main(string[] args)
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    public class Program
     {
-      Checker check = new Checker();
+        public static void Main(string[] args)
+        {
+            Checker check = new Checker();
 
-      Console.WriteLine(check.DayOfWeek("tue"));
-       Console.WriteLine(check.DayOfWeek("tues"));
+            Console.WriteLine(check.DayOfWeek("tue"));
+            Console.WriteLine(check.DayOfWeek("tues"));
 
-      Console.WriteLine(check.AllVowels("aeiouaaeeioiouoiaoueaiaeiou"));
-      Console.WriteLine(check.AllVowels("aeiouaaeeioiouoKiaoueaiaeiou"));
+            Console.WriteLine(check.AllVowels("aeiouaaeeioiouoiaoueaiaeiou"));
+            Console.WriteLine(check.AllVowels("aeiouaaeeioiouoKiaoueaiaeiou"));
 
-      Console.WriteLine(check.TimeOfDay("23:23:59"));
-      Console.WriteLine(check.TimeOfDay("00:00:60"));
+            Console.WriteLine(check.TimeOfDay("23:23:59"));
+            Console.WriteLine(check.TimeOfDay("00:00:60"));
+        }
     }
-  }
 }
--- part10/exercise_159/src/Exercise/Regex/Checker.cs
+++ part10/exercise_159/src/Exercise/Regex/Checker.cs
@@ -1,21 +1,45 @@
 namespace Exercise
 {
-  using System.Text.RegularExpressions;
-  public class Checker
-  {
-    public bool DayOfWeek(string str)
+    using System.Text.RegularExpressions;
+    public class Checker
     {
-      return false;
-    }
+        public bool DayOfWeek(string str)
+        {
+            Regex regex = new Regex("^(mon|tue|wed|thu|fri|sat|sun)$");
+            if (regex.IsMatch(str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-    public bool AllVowels(string str)
-    {
-      return false;
-    }
+        public bool AllVowels(string str)
+        {
+            Regex regex = new Regex("^(a|e|i|o|u|A|E|I|O|U)*$");
+            if (regex.IsMatch(str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-    public bool TimeOfDay(string str)
-    {
-      return false;
+        public bool TimeOfDay(string str)
+        {
+            Regex regex = new Regex("^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$");
+            if (regex.IsMatch(str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
-  }
 }
