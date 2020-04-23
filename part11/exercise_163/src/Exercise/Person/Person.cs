3a66c2f8b29fc1d1047d8fee30eb910e5b54d80a
staffanholmnas <staffan.holmnas@centria.fi>
2020-04-23 21:45:05 +0300

    Exercise 163 Done

--- part11/exercise_163/src/Exercise/Person/Person.cs
+++ part11/exercise_163/src/Exercise/Person/Person.cs
@@ -1,16 +1,25 @@
 namespace Exercise
 {
-  using System;
-  public class Person
-  {
-
-    public string name { get; }
-    public int age { get; }
-
-    public Person(string name, int age)
+    using System;
+    public class Person
     {
-      this.name = name;
-      this.age = age;
+
+        public string name { get; }
+        public int age { get; }
+
+        public Person(string name, int age)
+        {
+           
+            if (name == null || name == "" || name.Length > 40)
+            {
+                throw new ArgumentException("Give a real name.");
+            }
+            if (age < 0 || age > 120)
+            {
+                throw new ArgumentException("Give an age between 0 and 120.");
+            }
+            this.name = name;
+            this.age = age;
+        }
     }
-  }
 }
