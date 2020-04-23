46a541aaa57f058b5900eaab9e0d4bc71aa69bba
staffanholmnas <staffan.holmnas@centria.fi>
2020-04-23 21:15:47 +0300

    Exercise 162 Done

--- part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
+++ part11/exercise_162/src/Exercise/Lottery/LotteryRow.cs
@@ -1,34 +1,48 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
+    using System;
+    using System.Collections.Generic;
 
-  public class LotteryRow
-  {
-    private List<int> numbers;
-
-    public LotteryRow()
+    public class LotteryRow
     {
-      this.RandomizeNumbers();
-    }
+        private List<int> numbers;
 
-    public List<int> Numbers()
-    {
-      return this.numbers;
-    }
+        public LotteryRow()
+        {
+            this.RandomizeNumbers();
+        }
 
-    public bool ContainsNumber(int number)
-    {
-      // Tests whether the number is already among the randomized numbers
-      return false;
-    }
+        public List<int> Numbers()
+        {
+            return this.numbers;
+        }
 
-    public void RandomizeNumbers()
-    {
-      // initialize the list for numbers
-      this.numbers = new List<int>();
-      // Implement the randomization of the numbers by using the method ContainsNumber() here
-    }
+        public bool ContainsNumber(int number)
+        {
+            // Tests whether the number is already among the randomized numbers
+            if (this.numbers.Contains(number))
+            {
+                return true;
+            }
+            return false;
+        }
 
-  }
+        public void RandomizeNumbers()
+        {
+            // initialize the list for numbers
+            this.numbers = new List<int>();
+            // Implement the randomization of the numbers by using the method ContainsNumber() here
+            Random lotto = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                int lottoNumber = lotto.Next(1, 41);
+                while (ContainsNumber(lottoNumber))
+                {
+                    lottoNumber = lotto.Next(1, 41);
+                }
+                this.numbers.Add(lottoNumber);
+            }
+            this.numbers.Sort();
+        }
+    }
 }
