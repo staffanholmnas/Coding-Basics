eecfe760089b4ddff287d4bf93ad99181c8c36a0
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-05 19:37:41 +0300

    Exercise 162 Done

--- part11/exercise_162/src/Exercise/Program.cs
+++ part11/exercise_162/src/Exercise/Program.cs
@@ -1,21 +1,21 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
-  public class Program
-  {
-    public static void Main(string[] args)
+    using System;
+    using System.Collections.Generic;
+    public class Program
     {
-      LotteryRow row = new LotteryRow();
-      List<int> lotteryNumbers = row.Numbers();
+        public static void Main(string[] args)
+        {
+            LotteryRow row = new LotteryRow();
+            List<int> lotteryNumbers = row.Numbers();
 
-      Console.WriteLine("Lottery numbers:");
-      foreach (int number in lotteryNumbers)
-      {
-        Console.Write(number + " ");
-      }
+            Console.WriteLine("Lottery numbers:");
+            foreach (int number in lotteryNumbers)
+            {
+                Console.Write(number + " ");
+            }
 
-      Console.WriteLine("");
+            Console.WriteLine("");
+        }
     }
-  }
 }
