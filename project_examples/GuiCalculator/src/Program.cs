348b13027d9d050e24c90f1f923e069fbf364063
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-05 23:33:03 +0300

    Edit Calculator

--- project_examples/GuiCalculator/src/Program.cs
+++ project_examples/GuiCalculator/src/Program.cs
@@ -1,20 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GuiCalculator
 {
-  static class Program
-  {
-    [STAThread]
-    static void Main()
+    static class Program
     {
-      Application.SetHighDpiMode(HighDpiMode.SystemAware);
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Calculator());
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Calculator());
+        }
     }
-  }
 }
