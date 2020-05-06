431b263768a701566165dfe8726b3d8224b06e51
staffanholmnas <staffan.holmnas@centria.fi>
2020-05-06 09:52:13 +0300

    Project Examples Done

--- project_examples/GuiProject/src/Form1.Designer.cs
+++ project_examples/GuiProject/src/Form1.Designer.cs
@@ -1,60 +1,78 @@
-﻿namespace src
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace GuiProject
 {
-  partial class Form1
-  {
-    private System.ComponentModel.IContainer components = null;
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-      if (disposing && (components != null))
-      {
-        components.Dispose();
-      }
-      base.Dispose(disposing);
+        private IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.components = new Container();
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(800, 600);
+            this.Text = "Staffans App";
+            this.MinimumSize = new Size(800, 400);
+            this.MaximumSize = new Size(800, 1000);
+            this.MaximizeBox = false;
+            this.CenterToScreen();
+
+            this.textBox1 = new TextBox();
+            this.textBox1.Size = new Size(150, 600);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Location = new Point((this.Width - this.textBox1.Width) / 2, (this.Height - this.textBox1.Height) / 2);
+            this.Controls.Add(this.textBox1);
+
+            this.button1 = new Button();
+            this.button1.DialogResult = DialogResult.OK;
+            this.button1.Location = new Point((this.Width - this.textBox1.Width) / 2, 150);
+            this.button1.Height = 40;
+            this.button1.Width = 150;
+            this.button1.Text = "Click here!";
+            this.button1.Name = "button1";
+            this.button1.Font = new Font("Helvetica", 16);
+            this.button1.BackColor = Color.LightSkyBlue;
+            this.button1.ForeColor = Color.Black;
+            this.button1.Click += new EventHandler(ShowMessage);
+            Controls.Add(button1);
+
+            this.button2 = new Button();
+            this.button2.DialogResult = DialogResult.OK;
+            this.button2.Location = new Point((this.Width - this.textBox1.Width) / 2, this.button1.Height + 150);
+            this.button2.Height = 40;
+            this.button2.Width = 150;
+            this.button2.Text = "No, click me!";
+            this.button2.Name = "button2";
+            this.button2.Font = new Font("Helvetica", 16);
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = AutoSizeMode.GrowOnly;
+            this.button2.BackColor = Color.PaleTurquoise;
+            this.button2.ForeColor = Color.Black;
+            this.button2.Click += new EventHandler(ShowMessage);
+            Controls.Add(button2);
+        }
+        private void ShowMessage(object sender, EventArgs e)
+        {
+            string buttonName = (sender as Button).Text;
+            if (buttonName == "Click here!")
+            {
+                this.textBox1.Text = "Button Clicked!";
+            }
+            else
+            {
+                this.textBox1.Text = "Other Button Clicked!";
+            }
+        }
     }
-
-    private void InitializeComponent()
-    {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Hello World Application";
-
-      this.textBox1 = new System.Windows.Forms.TextBox();
-
-      this.textBox1.Text = "Hello World";
-      this.textBox1.ReadOnly = true;
-      this.textBox1.Location = new System.Drawing.Point((this.Width - this.textBox1.Width) / 2, (this.Height - this.textBox1.Height) / 2);
-      this.Controls.Add(this.textBox1);
-
-      this.button1 = new System.Windows.Forms.Button();
-      this.button1.Text = "Click me!";
-      this.button1.Click += new System.EventHandler(ShowMessage);
-      Controls.Add(this.button1);
-
-      this.button2 = new System.Windows.Forms.Button();
-      this.button2.Text = "Click me instead!";
-      this.button2.AutoSize = true;
-      this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowOnly;
-      this.button2.Location = new System.Drawing.Point(this.button1.Width + 5, 0);
-      this.button2.Click += new System.EventHandler(ShowMessage);
-      Controls.Add(this.button2);
-
-
-    }
-    private void ShowMessage(object sender, System.EventArgs e)
-    {
-      string buttonName = (sender as System.Windows.Forms.Button).Text;
-      if (buttonName == "Click me!")
-      {
-        this.textBox1.Text = "Button Clicked!";
-      }
-      else
-      {
-        this.textBox1.Text = "Other Button Clicked!";
-      }
-
-    }
-
-  }
 }
 
